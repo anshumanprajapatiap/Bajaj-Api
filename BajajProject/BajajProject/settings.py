e2c25094a508c5f8dfae2ff89186597eda39588d
Anshuman Prajapati <44315336+anshumanprajapatiap@users.noreply.github.com>
2021-08-03 12:59:11 +0530

    up

--- BajajProject/BajajProject/settings.py
+++ BajajProject/BajajProject/settings.py
@@ -119,8 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 #DISABLE_COLLECTSTATIC=1
-STATI_ROOT = os.path.join(BASE_DIR,'static')
-STATIC_URL = '/static/'
+#STATI_ROOT = os.path.join(BASE_DIR,'static')
+#STATIC_URL = '/static/'
 
 
 # Default primary key field type
